3feb8db744d85e8d34f0736eb04fbe13d81d7ece
Praxis | Darian <hello@vulpinecat.com>
2022-11-05 15:09:54 +0100

    add application tests

--- src/utils/application.rs
+++ src/utils/application.rs
@@ -45,14 +45,11 @@ impl Application {
         self
     }
 
-    fn is_image_related(&mut self, image: &Image) -> bool {
+    fn is_image_related(&self, image: &Image) -> bool {
         let Image { created_from, .. } = image;
 
         match (created_from.clone(), self.name.clone()) {
-            (Some(source), Some(name)) if source.name == name => {
-                self.add_image(image);
-                true
-            }
+            (Some(source), Some(name)) if source.name == name => true,
             _ => false,
         }
     }
@@ -104,11 +101,12 @@ pub fn generate_application_list(
         let mut assinged = false;
         for app in &mut vec {
             if app.is_image_related(image) {
+                app.add_image(image);
                 assinged = true;
-                continue;
+                break;
             }
         }
-        // Break if we assinged in inner loop
+        // Continue if we assinged in inner loop
         if assinged {
             continue;
         }
@@ -118,4 +116,110 @@ pub fn generate_application_list(
     }
 
     vec
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Application;
+    use hcloud::models::{
+        image::{OsFlavor, Status, Type},
+        CreatedFrom, Image, Protection,
+    };
+    use std::collections::HashMap;
+
+    fn empty_snapshot() -> Image {
+        Image {
+            bound_to: None,
+            created: String::new(),
+            created_from: None,
+            deleted: None,
+            deprecated: None,
+            description: "".to_string(),
+            disk_size: 0.0,
+            id: 0,
+            image_size: None,
+            labels: HashMap::new(),
+            name: None,
+            os_flavor: OsFlavor::Unknown,
+            os_version: None,
+            protection: Box::new(Protection { delete: false }),
+            rapid_deploy: None,
+            status: Status::Available,
+            r#type: Type::Snapshot,
+        }
+    }
+
+    mod application {
+        use super::{empty_snapshot, Application, CreatedFrom};
+
+        impl PartialEq for Application {
+            fn eq(&self, other: &Self) -> bool {
+                self.id == other.id
+                    && self.name == other.name
+                    && self.status == other.status
+                    && self.images == other.images
+                    && self.servers == other.servers
+            }
+        }
+
+        #[test]
+        fn is_image_related() {
+            let unrelated_image = empty_snapshot();
+            let application = Application {
+                id: 0,
+                name: Some("unique".to_string()),
+                status: None,
+                images: None,
+                servers: None,
+            };
+
+            let mut related_image = unrelated_image.clone();
+            related_image.created_from = Some(Box::new(CreatedFrom {
+                id: 0,
+                name: "unique".to_string(),
+            }));
+
+            assert!(application.is_image_related(&related_image));
+            assert!(!application.is_image_related(&unrelated_image));
+        }
+
+        #[test]
+        fn with_image() {
+            let mut image = empty_snapshot();
+            image.name = Some("unique".to_string());
+
+            let application = Application::new().with_image(&image);
+            let control_application = Application {
+                id: application.id,
+                name: Some("unique".to_string()),
+                status: Some("Available".to_string()),
+                images: Some(vec![image]),
+                servers: None,
+            };
+
+            assert_eq!(application, control_application);
+        }
+
+        #[test]
+        fn with_image_unnamed() {
+            let image = empty_snapshot();
+
+            let application = Application::new().with_image(&image);
+            let mut control_application = Application {
+                id: application.id,
+                name: Some(format!("unnamed {}", application.id)),
+                status: Some("Available".to_string()),
+                images: Some(vec![image.clone()]),
+                servers: None,
+            };
+
+            assert_eq!(application, control_application);
+
+            // Ensure that another unnamed application will have a new id
+            let another_application = Application::new().with_image(&image);
+            control_application.id += 1;
+            control_application.name = Some(format!("unnamed {}", application.id + 1));
+            assert_eq!(another_application, control_application)
+        }
+    }
 }
